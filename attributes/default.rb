bf84f0ecfd46d4c28c42ba6d4afc5381bf671476
Aaron Baer <aaron@hw-ops.com>
2014-04-24 10:45:38 -0700

    install and configure aptly and aptly.conf

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,19 @@ default['aptly']['dist'] = "squeeze"
 default['aptly']['components'] = ['main']
 default['aptly']['keyserver'] = 'keys.gnupg.net'
 default['aptly']['key'] = '2A194991'
+
+default['aptly']['user'] = 'aptly'
+default['aptly']['group'] = 'aptly'
+
+default['aptly']['rootdir'] = "/opt/aptly"
+default['aptly']['downloadconcurrency'] = 4
+default['aptly']['architectures'] = []
+default['aptly']['dependencyfollowsuggests'] = false
+default['aptly']['dependencyfollowrecommends'] = false
+default['aptly']['dependencyfollowallvariants'] = false
+default['aptly']['dependencyfollowsource'] = false
+default['aptly']['gpgdisablesign'] = false
+default['aptly']['gpgdisableverify'] = false
+default['aptly']['downloadsourcepackages'] = false
+default['aptly']['ppadistributorid'] = ""
+default['aptly']['ppacodename'] = ""
--- recipes/install.rb
+++ recipes/install.rb
@@ -29,3 +29,44 @@ apt_repository "aptly" do
 end
 
 package "aptly"
+package "graphviz"
+
+group node['aptly']['group'] do
+  action :create
+end
+
+user node['aptly']['user'] do
+  gid node['aptly']['group']
+  shell '/bin/bash'
+  home node['aptly']['rootdir']
+  system true
+  action :create
+end
+
+directory node['aptly']['rootdir'] do
+  owner node['aptly']['user']
+  group node['aptly']['group']
+  mode  00755
+  action :create
+end
+
+template '/etc/aptly.conf' do
+  source 'aptly.conf.erb'
+  owner 'root'
+  group 'root'
+  mode  00644
+  variables({
+    :rootdir => node['aptly']['rootdir'],
+    :downloadconcurrency => node['aptly']['downloadconcurrency'],
+    :architectures => node['aptly']['architectures'],
+    :dependencyfollowsuggests => node['aptly']['dependencyfollowsuggests'],
+    :dependencyfollowrecommends => node['aptly']['dependencyfollowrecommends'],
+    :dependencyfollowallvariants => node['aptly']['dependencyfollowallvariants'],
+    :dependencyfollowsource => node['aptly']['dependencyfollowsource'],
+    :gpgdisablesign => node['aptly']['gpgdisablesign'],
+    :gpgdisableverify => node['aptly']['gpgdisableverify'],
+    :downloadsourcepackages => node['aptly']['downloadsourcepackages'],
+    :ppadistributorid => node['aptly']['ppadistributorid'],
+    :ppacodename => node['aptly']['ppacodename']
+  })
+end
