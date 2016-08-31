d455db0a694d2caa58f48a940f8d812a51a22e45
Andrew Jones <andrew@jones.ec>
2016-08-31 16:00:10 -0400

    Add support for tarball installations

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,23 @@ default['aptly']['components'] = ['main']
 default['aptly']['keyserver'] = 'keys.gnupg.net'
 default['aptly']['key'] = '9E3E53F19C7DE460'
 
+default['aptly']['tarball']['uri'] =
+  'https://bintray.com/artifact/download/smira/aptly/' \
+  'aptly_0.9.7_linux_amd64.tar.gz'
+
+default['aptly']['tarball']['checksum'] =
+  'ec877942783c7a24566c802120da51d4cecaf2c76d3151e1a4869da1ee0690f7'
+
+default['aptly']['tarball']['version'] =
+  begin
+    require 'uri'
+    filename =
+      ::File.basename(URI.parse(node['aptly']['tarball']['uri']).path)
+    /aptly_(.+?)_/.match(filename)[1]
+  rescue
+    1
+  end
+
 default['aptly']['user'] = 'aptly'
 default['aptly']['group'] = 'aptly'
 
