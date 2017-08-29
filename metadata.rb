5ae952a61df33ac8736269c934d85d334afc735e
Tim Smith <tsmith@chef.io>
2017-08-29 10:07:46 -0700

    Add new metadata to resolve foodcritic warnings
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'aptly'
 maintainer       'Heavy Water Operations, LLC'
 maintainer_email 'aaronb@heavywater.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures aptly'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.9'
@@ -10,3 +10,7 @@ supports 'ubuntu'
 supports 'debian'
 
 depends 'apt'
+
+source_url 'https://github.com/hw-cookbooks/aptly' if respond_to?(:source_url)
+issues_url 'https://github.com/hw-cookbooks/aptly/issues' if respond_to?(:issues_url)
+chef_version '>= 11' if respond_to?(:chef_version)
