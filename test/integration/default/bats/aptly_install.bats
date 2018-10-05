9802760089726d10d0664dc1eb028cb2ccad052e
Dan Webb <dan.webb@damacus.io>
2018-10-05 20:27:29 +0100

    Update cookbook to sous-chefs standards

--- test/integration/default/bats/aptly_install.bats
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bats
-
-@test "aptly binary is found" {
-  which aptly
-}
--- test/integration/default/cookbooks/aptly_test/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'aptly_test'
-maintainer       'Heavy Water Operations, LLC'
-maintainer_email 'aaron@hw-ops.com'
-license          'Apache 2.0'
-description      'Installs/Configures aptly_test'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-depends 'aptly'
