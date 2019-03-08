0c5af5f25c2987518b83c1daa7c8638c36dc2606
Elias Abacioglu <elias.rabi@gmail.com>
2019-03-08 15:50:16 +0100

    v1.1.0 release (#51)

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
-name 'aptly'
-maintainer 'Heavy Water Operations, LLC'
-maintainer_email 'aaronb@heavywater.io'
-license 'Apache-2.0'
-description 'Installs/Configures aptly'
+name             'aptly'
+maintainer       'Sous Chefs'
+maintainer_email 'help@sous-chefs.org'
+license          'Apache-2.0'
+description      'Installs/Configures aptly'
 long_description 'Installs/Configures aptly'
-version '1.0.0'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+version          '1.1.0'
+chef_version     '>= 13.0' if respond_to?(:chef_version)
 
 supports 'ubuntu'
 supports 'debian'
