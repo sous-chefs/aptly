895eedea2031539219031fb0892ea181417cee43
Jason Field <jason@avon-lea.co.uk>
2019-10-19 13:07:58 +0100

    Release 2.0.1
    Releases cookbook version 2.0.1 with latest cookstyle fixes
    
    Signed-off-by: Jason Field <jason@avon-lea.co.uk>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,9 @@ maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs/Configures aptly'
-
 issues_url        'https://github.com/sous-chefs/aptly/issues'
 source_url        'https://github.com/sous-chefs/aptly'
-version           '2.0.0'
+version           '2.0.1'
 chef_version      '>= 13.0'
 
 supports 'ubuntu'
