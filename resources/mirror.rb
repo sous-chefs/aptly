97ebf2865a643ce7e209650b85b5af5fef09df2e
Dan Webb <dan.webb@damacus.io>
2021-06-11 15:05:41 +0100

    Unified Mode
    - Set unified_mode to true for Chef 17 support
    - Set minium Chef version to 15.3 for unified_mode

--- resources/mirror.rb
+++ resources/mirror.rb
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+unified_mode true
 property :mirror_name,             String, name_property: true
 property :component,               String, default: ''
 property :distribution,            String, default: ''
